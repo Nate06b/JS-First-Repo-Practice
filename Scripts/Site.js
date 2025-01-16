15f8f19b930a2d32b0bbd7eb41f8604eeff4aa60
Beardslee <500210725@fvtc.edu>
2025-01-16 13:22:19 -0600

    First Commit

---
+++ Scripts/Site.js
@@ -0,0 +1,18 @@
+
+const seconds = (new Date()).getSeconds()
+
+const p = document.querySelector('p')
+
+setTimeout( () => {
+p.textContent = 'General Kenobi!' + seconds
+}, 2000)
+
+setTimeout( () => {
+if (seconds % 2 == 1)
+{
+    p.style.color = '#990000'    
+}
+else{
+    p.style.color = '#00CC00'
+}
+}, 3000)
